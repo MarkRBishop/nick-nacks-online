d3cc244b465ebe47fc7d44ebbe17826d0fb2bdcc
MarkRBishop <mrb.8@hotmail.com>
2024-01-03 16:01:38 -0600

    removed instructional comments

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const products = await Product.findAll({
       include: [
@@ -23,7 +22,6 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const product = await Product.findByPk(req.params.id, {
       include: [
